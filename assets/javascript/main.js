c174cbc3604bd6454c025186fadeb315817f689f
Robert Lester <Lesters.robert@gmail.com>
2017-03-15 18:25:20 -0400

    added main page images

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDWjD9kZIz9VNzDOyOT5d9dG0d6cbRDozU",
+        authDomain: "be-selekt.firebaseapp.com",
+        databaseURL: "https://be-selekt.firebaseio.com",
+        storageBucket: "be-selekt.appspot.com",
+        messagingSenderId: "274079531363"
+    };
+
+    firebase.initializeApp(config);
+
+
 })
