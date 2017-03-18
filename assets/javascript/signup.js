cd52d9fded19fc80ff2d99f4d300277d2e183da7
AJ Ford <AJCranford1019@gmail.com>
2017-03-18 13:50:35 -0400

    Authenticate Signup Working

--- assets/javascript/signup.js
+++ assets/javascript/signup.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
-    // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDWjD9kZIz9VNzDOyOT5d9dG0d6cbRDozU",
-    authDomain: "be-selekt.firebaseapp.com",
-    databaseURL: "https://be-selekt.firebaseio.com",
-    storageBucket: "be-selekt.appspot.com",
-    messagingSenderId: "274079531363"
-  };
-  firebase.initializeApp(config);
+
 
   var database = firebase.database();
 
 
-var global_username = '';
 
+var email = $('#userEmail').val().trim();
+var password = $('#userPassword').val();
+
+$('#createAccount').on('click', function(){
+var email = $('#userEmail').val().trim();
+var password = $('#userPassword').val();
+
+	console.log(email);
+	console.log(password);
 
 firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
   // Handle Errors here.
@@ -22,5 +22,7 @@ firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(e
   var errorMessage = error.message;
   // ...
 });
+
+})
 
 })
