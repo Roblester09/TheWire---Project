3fd9cb05b7b0daed3a51cc3e3bfcfb52ed727160
Isaac Ivins <isaacivins@Isaacs-MacBook-Air.local>
2017-03-18 11:50:35 -0400

    gmaps

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,5 +11,52 @@ $(document).ready(function() {
 
     firebase.initializeApp(config);
 
+    var geoLocateMapsAPI = "AIzaSyAu2voEOxFGHtqYtNUUIST0Zl-SdLBWLYY"
+
+    var baseURL = "https://www.googleapis.com/geolocation/v1/geolocate?key="
+
+      function initMap() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        if (navigator.geolocation) {
+
+          navigator.geolocation.getCurrentPosition(function(position) {
+          	//places lat and long attributes to current location
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            map.setCenter(pos);
+            
+            //places marker on position
+            var marker = new google.maps.Marker({
+            	position: pos,
+            	map: map
+            })
+
+          }, function() {
+
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+
+        } else {
+
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+          'Error: The Geolocation service failed.' :
+          'Error: Your browser doesn\'t support geolocation.');
+      }
+
+
 
 })
