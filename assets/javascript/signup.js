c51a31796f4bea2b17d9101a2399134f590b872c
AJ Ford <AJCranford1019@gmail.com>
2017-03-20 16:24:10 -0400

    ID Change and Sign in authentication

--- assets/javascript/signup.js
+++ assets/javascript/signup.js
@@ -4,11 +4,6 @@ $(document).ready(function() {
 
   var database = firebase.database();
 
-
-
-var email = $('#userEmail').val().trim();
-var password = $('#userPassword').val();
-
 $('#createAccount').on('click', function(){
 var email = $('#userEmail').val().trim();
 var password = $('#userPassword').val();
@@ -17,6 +12,19 @@ var password = $('#userPassword').val();
 	console.log(password);
 
 firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+})
+
+$('#account-login').on('click', function(){
+	var email = $("#loginEmail").val().trim();
+	var password = $('#loginPassword').val();
+
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
