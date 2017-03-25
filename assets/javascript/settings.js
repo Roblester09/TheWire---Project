b08145532e9fe1406e7ef0404cfac34d53f65cb9
AJ Ford <AJCranford1019@gmail.com>
2017-03-24 23:53:12 -0400

    File Uploading

--- assets/javascript/settings.js
+++ assets/javascript/settings.js
@@ -50,7 +50,67 @@ var facebookURL = $('#facebook').val().trim();
 var instagramURL = $('#instagram').val().trim();
 var linkedInURL = $('#linkedIn').val().trim();
 
-// var profile_picture = $('#inputFile')
+ 
+    $('input[type=file]').change(function(){
+ 
+        $(this).simpleUpload( {
+ 
+            start: function(file){
+                //upload started 
+                $('#filename').html(file.name);
+                $('#progress').html("");
+                $('#progressBar').width(0);
+            },
+ 
+            progress: function(progress){
+                //received progress 
+                $('#progress').html("Progress: " + Math.round(progress) + "%");
+                $('#progressBar').width(progress + "%");
+            },
+ 
+            success: function(data){
+                //upload successful 
+                $('#progress').html("Success!<br>Data: " + JSON.stringify(data));
+            },
+ 
+            error: function(error){
+                //upload failed 
+                $('#progress').html("Failure!<br>" + error.name + ": " + error.message);
+            }
+ 
+        });
+ 
+    });
+ 
+
+
+       var auth = firebase.auth();
+        var storageRef = firebase.storage().ref();
+      // Push to child path.
+      // [START oncomplete]
+      storageRef.child('images/' + userId + '/' + file.name).put(file).then(function(snapshot) {
+        console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+        console.log(snapshot.metadata);
+        var url = snapshot.downloadURL;
+        console.log('File available at', url);
+        // [START_EXCLUDE]
+        // [END_EXCLUDE]
+      }).catch(function(error) {
+        // [START onfailure]
+        console.error('Upload failed:', error);
+        // [END onfailure]
+      });
+      // [END oncomplete]
+    
+
+    // Create a reference to 'mountains.jpg'
+        var profilePictureRef = storageRef.child(file.name);
+
+// Create a reference to 'images/mountains.jpg'
+        var profilePicture = storageRef.child('images/' + userId + '/' + file.name);
+
+setTimeout(function(){
+     location.reload(); }, 2000);
 
 
   var userId = firebase.auth().currentUser.uid;
@@ -66,7 +126,6 @@ var linkedInURL = $('#linkedIn').val().trim();
     facebookURL: facebookURL,
     instagramURL: instagramURL,
     linkedInURL: linkedInURL,
-    // profilePicture : imageUrl
   });
 })
 
