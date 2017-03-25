8efd54b05214bdb0bfb7e998a44ca89e67f9ead5
AJ Ford <AJCranford1019@gmail.com>
2017-03-25 01:34:33 -0400

    Trial And Error File Upload

--- assets/javascript/settings.js
+++ assets/javascript/settings.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 var database = firebase.database();
+var selectefFile;
 
 
 firebase.auth().onAuthStateChanged(function(user) {
@@ -33,9 +34,46 @@ firebase.auth().onAuthStateChanged(function(user) {
     }
  });
 
+// $('#file').on('change', function(){
+//     var selectefFile = event.target.files[0];
+// });
 
+// $('#uploadFiles').on('click', function(){
 
+//     var userId = user.uid
+//     var storageRef = firebase.storage().ref('/' + userId + '/' + fileName);
+//     var fileName = selectefFile.name;
+//     var uploadTask = storageRef.put(selectefFile);
 
+//         // Register three observers:
+//     // 1. 'state_changed' observer, called any time the state changes
+//     // 2. Error observer, called on failure
+//     // 3. Completion observer, called on successful completion
+//     uploadTask.on('state_changed', function(snapshot){
+//       // Observe state change events such as progress, pause, and resume
+//       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+//       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//       console.log('Upload is ' + progress + '% done');
+//       switch (snapshot.state) {
+//         case firebase.storage.TaskState.PAUSED: // or 'paused'
+//           console.log('Upload is paused');
+//           break;
+//         case firebase.storage.TaskState.RUNNING: // or 'running'
+//           console.log('Upload is running');
+//           break;
+//       }
+//     }, function(error) {
+//       // Handle unsuccessful uploads
+//     }, function() {
+//       // Handle successful uploads on complete
+//       // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+//       var downloadURL = uploadTask.snapshot.downloadURL;
+//       console.log(downloadURL);
+//     });
+
+// })
+
+ 
 
 $('#settingsSubmit').on('click', function writeUserData() {
   event.preventDefault();
@@ -50,67 +88,7 @@ var facebookURL = $('#facebook').val().trim();
 var instagramURL = $('#instagram').val().trim();
 var linkedInURL = $('#linkedIn').val().trim();
 
- 
-    $('input[type=file]').change(function(){
- 
-        $(this).simpleUpload( {
- 
-            start: function(file){
-                //upload started 
-                $('#filename').html(file.name);
-                $('#progress').html("");
-                $('#progressBar').width(0);
-            },
- 
-            progress: function(progress){
-                //received progress 
-                $('#progress').html("Progress: " + Math.round(progress) + "%");
-                $('#progressBar').width(progress + "%");
-            },
- 
-            success: function(data){
-                //upload successful 
-                $('#progress').html("Success!<br>Data: " + JSON.stringify(data));
-            },
- 
-            error: function(error){
-                //upload failed 
-                $('#progress').html("Failure!<br>" + error.name + ": " + error.message);
-            }
- 
-        });
- 
-    });
- 
 
-
-       var auth = firebase.auth();
-        var storageRef = firebase.storage().ref();
-      // Push to child path.
-      // [START oncomplete]
-      storageRef.child('images/' + userId + '/' + file.name).put(file).then(function(snapshot) {
-        console.log('Uploaded', snapshot.totalBytes, 'bytes.');
-        console.log(snapshot.metadata);
-        var url = snapshot.downloadURL;
-        console.log('File available at', url);
-        // [START_EXCLUDE]
-        // [END_EXCLUDE]
-      }).catch(function(error) {
-        // [START onfailure]
-        console.error('Upload failed:', error);
-        // [END onfailure]
-      });
-      // [END oncomplete]
-    
-
-    // Create a reference to 'mountains.jpg'
-        var profilePictureRef = storageRef.child(file.name);
-
-// Create a reference to 'images/mountains.jpg'
-        var profilePicture = storageRef.child('images/' + userId + '/' + file.name);
-
-setTimeout(function(){
-     location.reload(); }, 2000);
 
 
   var userId = firebase.auth().currentUser.uid;
